923caabbff8abaa7cabf56c81fbf8735d944195a
Akintomide Ayomide <akayomide11@gmail.com>
2026-06-05 02:14:41 +0100

    updated password reset page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,11 @@ import { Eye, EyeOff } from "lucide-react";
 import Link from "next/link"
 import { useState } from "react"
 import { RiMenu4Fill } from "react-icons/ri";
+import Image from "next/image";
+import { BiLogoFacebookCircle } from "react-icons/bi";
+import { FcGoogle } from "react-icons/fc";
+import { BsTwitterX } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa6";
 
 
 export default function LoginPage() {
@@ -10,8 +15,8 @@ export default function LoginPage() {
   const [hasError, setHasError] = useState(false)
 
   return (
-    <div className="min-h-screen  bg-linear-to-b from-[#1D1D1D] via-[#1D1D1D] via-5% to-[#000000] text-white flex flex-col">
-      <div className="w-full bg-linear-to-b from-[#1D1D1D] via-[#1D1D1D] via-15% to-[#000000]  flex flex-col min-h-screen">
+    <div className="min-h-screen  bg-linear-to-b from-[#1D1D1D] via-[#232222] via-10% to-[#181717] text-white flex flex-col">
+      <div className="w-full bg-linear-to-b from-[#1D1D1D] via-[#1e1e1e] via-15% to-[#000000]  flex flex-col min-h-screen">
     
         <div className="flex items-center justify-between px-5 py-4 border-b border-[#303030]">
           <RiMenu4Fill className="w-6 h-6 text-white" />
@@ -21,21 +26,21 @@ export default function LoginPage() {
         </div>
           <div className="flex flex-col flex-1 px-5 pt-8">
             <h2 className="font-inter text-[16px] font-bold leading-[1.2] tracking-[-0.02em] text-white mb-1 ml-2">Welcome to Korrect Trivia</h2>
-            <p className="text-[#888888] font-inter text-xs font-normal tracking-normal mb-8 mt-1 ml-2">
+            <p className="text-[#888888] font-inter text-[13.5px] font-normal tracking-normal mb-8 mt-1 ml-2">
               Kindly Login with your registered phone number
             </p>
-            <label className="text-[#888888] font-inter text-xs font-normal tracking-normal mb-2 mt-2">Phone number</label>
+            <label className="text-[#888888] font-inter text-[13.5px] font-normal tracking-normal mb-2 mt-2">Phone number</label>
             <input 
               type="tel"
               placeholder="+234 9000000001"
-              className="flex flex-row w-full max-w-90 h-fit rounded-[10px] border border-[#303030] p-[6px_8px] gap-1 bg-transparent text-white placeholder-zinc-500 outline-none transition-all duration-200 hover:border-[#D9E012] focus:border-[#D9E012] focus:ring-1 focus:ring-[#D9E012]"
+              className="flex flex-row w-full max-w-90 h-fit rounded-[10px] border border-[#383838] p-[6px_8px] gap-1 bg-[#222222] text-white placeholder-[#424141] outline-none transition-all duration-200 hover:border-[#D9E012] focus:border-[#D9E012] focus:ring-1 focus:ring-[#D9E012]"
             />
-            <label className="text-[#888888] font-inter text-xs font-normal tracking-normal mb-2 mt-4">Password</label>
+            <label className="text-[#888888] font-inter text-[13.5px] font-normal tracking-normal mb-2 mt-4">Password</label>
             <div className="relative mb-2">
               <input
                 type={showPassword ? "text" : "password"}
                 placeholder="********"
-                className="flex flex-row w-full max-w-90 h-fit rounded-[10px] border border-[#303030] p-[6px_8px] gap-1 bg-transparent text-white placeholder-zinc-500 outline-none transition-all duration-200 hover:border-[#D9E012] focus:border-[#D9E012] focus:ring-1 focus:ring-[#D9E012]"
+                className="flex flex-row w-full max-w-90 h-fit rounded-[10px] border border-[#383838] p-[6px_8px] gap-1 bg-[#222222] text-white placeholder-[#424141]  outline-none transition-all duration-200 hover:border-[#D9E012] focus:border-[#D9E012] focus:ring-1 focus:ring-[#D9E012]"
               />
               <button 
               className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400"
@@ -47,7 +52,7 @@ export default function LoginPage() {
  
 
               {hasError && (
-                <p className="text-[#FF7366] text-sm mb-2 mt-2">
+                <p className="text-[#FF7366] text-[12px] mb-2 mt-2">
                   Incorrect Password! Try again or Click forgot password to reset your password
                 </p>
               )}
@@ -62,11 +67,45 @@ export default function LoginPage() {
               </div>
 
               <button 
-                className="flex flex-row items-center justify-center w-88 h-11.5 rounded-xl p-[8px_24px] gap-2 bg-[#D9E012] text-[#1D1D1D] font-inter text-[16px] font-bold tracking-[-0.02em] transition-all duration-200 hover:bg-[#c2ca10] active:scale-[0.98] focus:outline-none focus:ring-2 focus:ring-[#D9E012]/50"
+                className="flex flex-row items-center justify-center w-88 h-11.5 rounded-xl p-[8px_24px] gap-2 bg-[#D9E012] text-[#3f3e3e] font-inter text-[14px] font-bold tracking-[-0.02em] transition-all duration-200 hover:bg-[#c2ca10] active:scale-[0.98] focus:outline-none focus:ring-2 focus:ring-[#D9E012]/50 mt-3"
                 onClick={() => setHasError(true)}
               >
                 Login
               </button>
+
+              <div className="flex items-center gap-3 mb-6 mt-10">
+                <div className="flex-1 h-px bg-[#303030]" />
+                <span className="text-[#ffffff] text-[12px]">Or Login with</span>
+                <div className="flex-1 h-px bg-[#303030]" />
+              </div>
+
+
+              <div className="flex gap-3 justify-center mt-3 ">
+
+              <button className="flex flex-row items-center w-fit h-fit rounded-[100px] border border-solid border-[#484848] p-[4px_8px_4px_4px] gap-1 text-white text-[13px] font-inter font-semibold">
+                <div className="flex items-center justify-center -mt-px -ml-px w-6 h-6 rounded-full border border-solid border-[#F6F2F2] bg-[#1877F2]">
+                    <FaFacebookF className="w-4 h-4 text-white cursor-pointer" />
+                  </div>
+                    Facebook
+                </button>
+
+               <button className="flex flex-row items-center w-fit h-fit rounded-[100px] border border-solid border-[#484848] p-[4px_8px_4px_4px] gap-1 text-white text-[13px] font-inter font-semibold">
+                  <div className="flex items-center justify-center -mt-px -ml-px w-6 h-6 rounded-full border border-solid border-[#F6F2F2] bg-white">
+                    <FcGoogle className="w-5.25 h-5.25 cursor-pointer" />
+                  </div>
+                    Google
+                </button>
+
+                <button className="flex flex-row items-center w-fit h-fit rounded-[100px] border border-solid border-[#484848] p-[4px_8px_4px_4px] gap-1 text-white text-[13px] font-inter font-semibold">
+                  <div className="flex items-center justify-center -mt-px -ml-px w-6 h-6 rounded-full border border-solid border-[#F6F2F2]">
+                    <BsTwitterX className="w-3 h-3 text-[#F6F2F2] cursor-pointer" />
+                  </div>
+                  Twitter
+                </button>
+                 
+                
+                
+              </div>
           </div>
       </div>
     </div>
---
+++ app/password-reset/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+
+
+
+export default function PasswordResetPage() {
+    const router = useRouter()
+
+    return (
+         <div className="min-h-screen bg-linear-to-b from-[#1D1D1D] via-[#232222] via-10% to-[#181717] text-white flex flex-col items-center">
+            <div className="w-full max-w-97.5 bg-linear-to-b from-[#1D1D1D] via-[#1e1e1e] via-15% to-[#000000] flex flex-col min-h-screen">
+
+
+        <div className="flex items-center justify-between px-5 py-4 border-b border-[#303030]">
+          <button onClick={() => router.back()}> 
+            <ChevronLeft className="w-5 h-5 text-white" />
+          </button>
+          <h1 className="text-sm font-semibold text-white">Reset Password</h1>
+          <div className="w-5" />
+        </div>
+
+        
+        <div className="flex flex-col flex-1 px-5 pt-8">
+            <h2 className="font-inter text-[16px] font-bold leading-[1.2] tracking-[-0.02em] text-white mb-1 ml-2">
+              Password reset  
+            </h2>
+            <p className="text-[#888888] font-inter text-[13.5px] font-normal tracking-normal mb-2 mt-2 ml-2">
+              Kindly enter the Phone number registered with your account and we will send you an OTP with instructions to reset your password  
+            </p>
+
+            <label className="text-[#888888] font-inter text-[13.5px] font-normal tracking-normal mb-2 mt-8">
+              Phone Number  
+            </label>
+            <input
+             type="tel"
+             placeholder="0000000001"
+             className="w-full rounded-[10px] border border-[#383838] p-[6px_8px] bg-[#222222] text-white placeholder-[#424141] bg outline-none transition-all duration-200 hover:border-[#D9E012] focus:border-[#D9E012] focus:ring-1 focus:ring-[#D9E012] mb-8"
+          />
+
+
+          <button
+          onClick={() => router.push("/create-password")}
+          className="w-full h-11.5 rounded-xl p-[8px_24px] bg-[#D9E012] text-[#3f3e3e] font-bold text-[14px] tracking-[-0.02em] transition-all duration-200 hover:bg-[#c2ca10] active:scale-[0.98] focus:outline-none focus:ring-2 focus:ring-[#D9E012]/50"
+          >
+            Send OTP
+          </button>
+
+        </div>
+
+
+        </div>
+    </div>
+
+    )
+}
